ab3c2e5ffbd2c14897af6ed61100c13c89c4a2d9
Morphineee29 <155790803+Morphineee29@users.noreply.github.com>
2025-06-04 17:12:24 +0700

    chore: init

---
+++ server/src/main/java/com/thinklab/platform/ThinklabPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.thinklab.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinklabPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinklabPlatformApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/thinklab/platform/ThinklabPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinklab.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinklabPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
